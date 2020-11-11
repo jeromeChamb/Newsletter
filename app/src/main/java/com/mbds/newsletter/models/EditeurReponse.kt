55d7e801c7b7bd2dc6e20cf56a01920bfff2950f
jChambord <j.chambord@net.estia.fr>
2020-11-11 10:33:48 +0100

    montre les editeurs

--- app/src/main/java/com/mbds/newsletter/models/EditeurReponse.kt
+++ app/src/main/java/com/mbds/newsletter/models/EditeurReponse.kt
@@ -1,6 +1,9 @@
 package com.mbds.newsletter.models
 
+import com.google.gson.annotations.SerializedName
+
 data class EditeurReponse(
     val status: String,
+    @SerializedName("sources")
     val editeur : List<Editeur>
 )
